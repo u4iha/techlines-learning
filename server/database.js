70229d1e6476e849d544173debeeaa150d3138da
ArtemShahanFenom <artem.shahan@fenomdigital.com>
2024-01-13 23:10:43 +0200

    server mongodb deprecated code deleted

--- server/database.js
+++ server/database.js
@@ -5,10 +5,7 @@ import mongoose from "mongoose";
 const connectToDatabase = async () => {
 	try {
 		mongoose.set("strictQuery", false);
-		const connect = await mongoose.connect(process.env.MONGO_URI, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-		});
+		const connect = await mongoose.connect(process.env.MONGO_URI);
 		console.log(`MongoDB connected: ${connect.connection.host}`);
 	} catch (error) {
 		console.log(`Error: ${error.message}`);
