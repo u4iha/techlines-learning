4583a9051c9950c30b901b91b9780cca7c4bf6fa
ArtemShahanFenom <artem.shahan@fenomdigital.com>
2024-01-14 01:00:05 +0200

    redux setup and products fetch from api

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -19,7 +19,7 @@ import { Link as ReactLink } from "react-router-dom";
 import { StarIcon } from "@chakra-ui/icons";
 import { useState } from "react";
 
-const Rating = ({ rating, numReviews }) => {
+const Rating = ({ rating, numberOfReviews }) => {
 	const { iconSize, setIconSize } = useState("14px");
 	return (
 		<Flex>
@@ -31,7 +31,7 @@ const Rating = ({ rating, numReviews }) => {
 				<StarIcon size={iconSize} w={"14px"} color={rating >= 5 ? "orange.500" : "gray.200"} />
 			</HStack>
 			<Text fontSize={"md"} fontWeight={"bold"} ml='4px'>
-				{`${numReviews} ${numReviews === 1 ? "Review" : "Reviews"}`}
+				{`${numberOfReviews} ${numberOfReviews === 1 ? "Review" : "Reviews"}`}
 			</Text>
 		</Flex>
 	);
@@ -49,7 +49,7 @@ const ProductCard = ({ product }) => {
 			rounded='lg'
 			position={"relative"}
 		>
-			{product.isNew && <Circle size='10px' position='absolute' top={2} right={2} bg='green.400' />}
+			{product.productIsNew && <Circle size='10px' position='absolute' top={2} right={2} bg='green.400' />}
 			{product.stock <= 0 && <Circle size='10px' position='absolute' top={2} right={2} bg='red.400' />}
 			<Image src={product.image} alt={product.name} roundedTop='lg' />
 			<Box flex='1' maxH='5' alignItems='baseline'>
@@ -58,7 +58,7 @@ const ProductCard = ({ product }) => {
 						Sold Out
 					</Badge>
 				)}
-				{product.isNew && (
+				{product.productIsNew && (
 					<Badge rounded='full' px='2px' fontSize='0.8em' colorScheme='green'>
 						New
 					</Badge>
@@ -72,7 +72,7 @@ const ProductCard = ({ product }) => {
 				</Link>
 			</Flex>
 			<Flex justifyContent={"space-between"} alignContent={"center"} py='2'>
-				<Rating rating={product} numReviews={product.numReviews} />
+				<Rating rating={product} numberOfReviews={product.numberOfReviews} />
 			</Flex>
 			<Flex justify={"space-between"}>
 				<Box fontSize={"2xl"} color={useColorModeValue("gray.800", "white")}>
